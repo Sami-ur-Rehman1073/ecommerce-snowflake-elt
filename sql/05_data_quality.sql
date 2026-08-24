66c471ecd982a92b55568a41f7d6d1ddefc92b70
Sami <sami.rehman1073@gmail.com>
2026-08-24 16:35:08 +0500

    Added quality checks for products

--- sql/05_data_quality.sql
+++ sql/05_data_quality.sql
@@ -17,3 +17,31 @@ SELECT
 FROM RAW.RAW_CUSTOMERS
 GROUP BY customer_id
 HAVING COUNT(*) > 1;
+
+
+-- NULL product IDs
+
+
+SELECT COUNT(*) AS null_product_ids
+FROM RAW.RAW_PRODUCTS
+WHERE product_id IS NULL;
+
+
+
+-- Invalid prices
+
+
+SELECT COUNT(*) AS invalid_prices
+FROM RAW.RAW_PRODUCTS
+WHERE unit_price <= 0;
+
+
+
+-- Duplicate products
+
+SELECT
+    product_id,
+    COUNT(*) AS duplicate_count
+FROM RAW.RAW_PRODUCTS
+GROUP BY product_id
+HAVING COUNT(*) > 1;
